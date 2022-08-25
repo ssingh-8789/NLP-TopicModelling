9356ba07d89d2bd7c7577395fc808020a3fa69b1
ssingh-8789 <107481157+ssingh-8789@users.noreply.github.com>
2022-08-25 18:36:33 +0530

    Update NMF1.py

--- NMF1.py
+++ NMF1.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
+#author name - shivani singh
+# author email - satyshi2015@gmail.com
 
 
 import pandas as pd
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import TfidfVectorizer
- 
-
-
-# In[4]:
-
 
 documents = pd.read_csv('news-data.csv', error_bad_lines=False)
 documents.head()
@@ -21,8 +14,6 @@ documents.head()
 # 2. The Features Matrix combines topics into documents
 # 
 # The first step is to create the TF-IDF matrix as follows.
-
-# In[5]:
 
 
 # use tfidf by removing tokens that don't appear in at least 50 documents
@@ -34,8 +25,6 @@ X = vect.fit_transform(documents.headline_text)
 
 # we will build the NMF model which will generate the Feature and the Component matrices.
 # 
-
-# In[6]:
 
 
 # Create an NMF instance: model
@@ -53,33 +42,20 @@ nmf_features = model.transform(X)
 # It important to check the dimensions of the 3 tables:
 
 # TF-IDF Dimensions:
-
-# In[7]:
-
-
 X.shape
 
 
 # Features Dimensions:
-
-# In[8]:
-
-
 nmf_features.shape
 
 
 # Components Dimensions:
-
-# In[9]:
-
-
 model.components_.shape
 
 
 # We should add the column names to the Components matrix since these are the tokens (words) from the TF-IDF.
 # based on our tokenizer we included any token of two or more characters that is why you will see some numbers. We could have also removed them. Both approaches are correct. 
 
-# In[10]:
 
 
 # Create a DataFrame: components_df
@@ -88,10 +64,6 @@ components_df
 
 
 # We have created the 10 topics using NMF. Let’s have a look at the 10 more important words for each topic.
-
-# In[11]:
-
-
 for topic in range(components_df.shape[0]):
     tmp = components_df.iloc[topic]
     print(f'For topic {topic+1} the words with the highest value are:')
@@ -104,18 +76,11 @@ for topic in range(components_df.shape[0]):
 # 2.Since we defined the topics, we will show how you can get the topic of each document. Let’s say that we want to get the topic of the 55th document: ‘funds to help restore cossack’
 # 
 # 
-
-# In[12]:
-
-
 my_document = documents.headline_text[55]
 my_document
 
 
 # We will need to work with the Features matrix. So let’s get the 55th row:
-
-# In[13]:
-
 
 pd.DataFrame(nmf_features).loc[55]
 
@@ -124,25 +89,13 @@ pd.DataFrame(nmf_features).loc[55]
 # 
 # Note that if we wanted to get the index in once, we could have typed:
 
-# In[14]:
-
 
 pd.DataFrame(nmf_features).loc[55].idxmax()
-
-
-
 # Finally, if we want to see the number of documents for each topic we can easily get it by typing:
-
-# In[15]:
-
-
 pd.DataFrame(nmf_features).idxmax(axis=1).value_counts()
 
 
 # Let’s say that we want to assign a topic of a new unseen document. Then, we will need to take the document, to transform the TF-IDF model and finally to transform the NMF model. Let’s take the actual new head title from ABC news.
-
-# In[17]:
-
 
 my_news = """NY's Sean Patrick Maloney wins primary over progressive challenger after moving districts """
  
@@ -152,19 +105,12 @@ X = vect.transform([my_news])
 nmf_features = model.transform(X)
  
 pd.DataFrame(nmf_features)
-
-
 # And if we want to get the index of the topic with the highest score:
 
-# In[18]:
-
-
 pd.DataFrame(nmf_features).idxmax(axis=1)
-
-
 # As expected, this document was classified as Topic 10 (with index 9).
 
-# In[ ]:
+
 
 
 
